cedc4d7ca03e8b7f532135f836c42b509b0b7310
Nikolai A <ilowen@ya.ru>
2023-08-16 22:11:08 +0800

    New: education-bem.css

--- src/scripts/education/mediaQuery.js
+++ src/scripts/education/mediaQuery.js
@@ -1,14 +1,28 @@
 window.addEventListener('DOMContentLoaded', (e) => {
-    const minWidth1440px = window.matchMedia('(min-width: 1440px)');
-    const maxWidth576px = window.matchMedia('(max-width: 576px)');
+    const leftbar = document.getElementById('leftbar');
+    const leftbarBtn = document.getElementById('leftbar-btn');
+    const menu = document.getElementById('menu');
 
-    // function handleMediaQueryChange() {
-    if (minWidth1440px.matches) {
-        menu.classList.remove('collapsed');
-    } else if (maxWidth576px.matches) {
-        menu.classList.remove('collapsed');
-    } else {
-        menu.classList.add('collapsed');
+    const minWidth1441px = window.matchMedia('(min-width: 1441px)').matches;
+    const maxWidth576px = window.matchMedia('(max-width: 576px)').matches;
+
+    function handleMediaQueryChange() {
+        if (minWidth1441px) {
+            expandLeftbar();
+        };
+
+        if (maxWidth576px) {
+            expandLeftbar();
+        };
+    }
+
+    handleMediaQueryChange();
+
+    // window.addEventListener('resize', handleMediaQueryChange);
+
+    function expandLeftbar() {
+        leftbar.classList.remove('leftbar_collapsed');
+        leftbarBtn.classList.add('leftbar__btn_active');
+        menu.classList.remove('menu_collapsed');
     };
-    // };
 });
--- src/scripts/education/onClick.js
+++ src/scripts/education/onClick.js
@@ -1,18 +1,64 @@
-const body = document.getElementById('body');
-const overlay = document.getElementById('overlay');
-const menu = document.getElementById('menu');
-const menuBtn = document.getElementById('menu-btn');
-const mobileMenuBtn = document.getElementById('mobile-menu-btn');
-
 document.addEventListener('click', (e) => {
-    if (e.target.id === 'menu-btn') {
-        menu.classList.toggle('collapsed');
-        menuBtn.classList.toggle('active');
+    const maxWidth576px = window.matchMedia('(max-width: 576px)').matches;
+
+    const overlay = document.getElementById('overlay');
+    const body = document.getElementById('body');
+
+    const leftbar = document.getElementById('leftbar');
+    const leftbarBtn = document.getElementById('leftbar-btn');
+
+    const rightbar = document.getElementById('rightbar');
+    const rightbarVisibility = window.getComputedStyle(rightbar).getPropertyValue('visibility');
+
+    const menu = document.getElementById('menu');
+
+    const personBtn = document.getElementById('person-btn');
+    const statsCardsWrapper = document.querySelector('.stats-cards__wrapper');
+
+    const toggleRightbarBtn = document.getElementById('toggleRightbarBtn');
+
+    const mobileLeftbarBtn = document.getElementById('mobile-leftbar-btn');
+    const mobileRightbarBtn = document.getElementById('mobile-rightbar-btn');
+    const mobileBtnsWrapper = document.querySelector('.mobile-btns-wrapper');
+
+    // collapse or expand leftbar
+    if (e.target === leftbarBtn) {
+        leftbar.classList.toggle('leftbar_collapsed');
+        menu.classList.toggle('menu_collapsed');
+
+        if (!leftbar.classList.contains('leftbar_collapsed')) {
+            leftbarBtn.classList.add('leftbar__btn_active');
+        } else {
+            leftbarBtn.classList.remove('leftbar__btn_active');
+        };
     };
 
-    if (e.target.id === 'mobile-menu-btn' || e.target.id === 'overlay') {
-        menu.classList.toggle('visible');
-        body.classList.toggle('disabled');
-        overlay.classList.toggle('visible');
+    // collapse or expand statsCards
+    if (e.target === personBtn) {
+        statsCardsWrapper.classList.toggle('stats-cards__wrapper_hidden');
+    };
+
+    // show or hide leftbar (mobile)
+    if (e.target === mobileLeftbarBtn || e.target === overlay) {
+        leftbar.classList.toggle('leftbar_visible');
+        overlay.classList.toggle('overlay_visible');
+        body.classList.toggle('body_disabled');
+        mobileBtnsWrapper.classList.toggle('mobile-btns-wrapper_hiddenGradient');
     }
+
+    // show or hide rightbar
+    if (e.target === toggleRightbarBtn || e.target === mobileRightbarBtn) {
+        if (rightbarVisibility === 'visible') {
+            rightbar.classList.remove('rightbar_visible');
+            rightbar.classList.add('rightbar_hidden');
+        } else {
+            rightbar.classList.remove('rightbar_hidden');
+            rightbar.classList.add('rightbar_visible');
+        };
+
+        if (maxWidth576px) {
+            overlay.classList.toggle('overlay_visible');
+            body.classList.toggle('body_disabled');
+        };
+    };
 });
