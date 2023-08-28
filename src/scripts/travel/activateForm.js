47947c8f739e6c55707c4a8c58b6f326b1362897
Nikolai A <ilowen@ya.ru>
2023-08-28 13:49:58 +0800

    Fix: travel/activateForm.js

--- src/scripts/travel/activateForm.js
+++ src/scripts/travel/activateForm.js
@@ -1,25 +1,11 @@
-const forms = document.querySelectorAll('form');
+const forms = document.querySelectorAll('.form');
 
-function deactivateAllForms() {
-    forms.forEach(e => {
-        e.classList.remove('active');
+forms.forEach(form => {
+    form.addEventListener('focusin', () => {
+        form.classList.add('active');
     });
-}
 
-document.addEventListener('click', (e) => {
-    if (e.target.tagName === 'INPUT') {
-        deactivateAllForms();
-        e.target.closest('.form').classList.add('active');
-    } else {
-        deactivateAllForms();
-    };
-});
-
-document.addEventListener('focusin', (e) => {
-    if (e.target.tagName === 'INPUT') {
-        deactivateAllForms();
-        e.target.closest('.form').classList.add('active');
-    } else {
-        deactivateAllForms();
-    };
+    form.addEventListener('focusout', () => {
+        form.classList.remove('active');
+    });
 });
